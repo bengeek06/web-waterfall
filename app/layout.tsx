89697c52fde33a2b67b8fe8eae003101858b4fc2
bengeek <bengeek06@gmail.com>
2025-08-31 21:49:40 +0200

    Add new SVG logo for Waterfall application

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
