a693d9b03615a52f9f5344b2d230e8e9277fb837
bengeek <bengeek06@gmail.com>
2025-09-01 00:07:39 +0200

    Implement i18n support for French and English; add dictionary files and update Home component to use localized strings

--- app/fr/page.tsx
+++ app/fr/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import { getDictionary } from "@/lib/dictionaries";
 
-export default function Home() {
+export default async function Home() {
+  const dictionary = await getDictionary('fr')
+
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <Image
@@ -10,10 +14,9 @@ export default function Home() {
         height={307}
         priority
       />
-      <h1 className="mt-4 text-2xl font-bold">Bienvenue sur Waterfall</h1>
+      <h1 className="mt-4 text-2xl font-bold">{dictionary.welcome}</h1>
       <p className="mt-2 text-lg text-gray-600">
-        Une application de gestion de projet qui permet aux utilisateurs de gérer
-        efficacement les projets, les jalons et les livrables.
+        {dictionary.description}
       </p>
       <p className="mt-2 text-lg text-gray-600">
         LOG_LEVEL is set to: {process.env.LOG_LEVEL || "info"}
