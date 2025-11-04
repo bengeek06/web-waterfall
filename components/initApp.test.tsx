414cf0526ae295689bc52f153115aa536f9b30d0
bengeek <bengeek06@gmail.com>
2025-11-04 23:03:51 +0100

    feat: add dual licensing information to multiple components for compliance and clarity

--- components/initApp.test.tsx
+++ components/initApp.test.tsx
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2025 Waterfall
+ * 
+ * This source code is dual-licensed under:
+ * - GNU Affero General Public License v3.0 (AGPLv3) for open source use
+ * - Commercial License for proprietary use
+ * 
+ * See LICENSE and LICENSE.md files in the root directory for full license text.
+ * For commercial licensing inquiries, contact: benjamin@waterfall-project.pro
+ */
+
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { useRouter } from 'next/navigation';
